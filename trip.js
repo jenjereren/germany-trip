3b7a803340bc82bb7fa74609ef61587704f3b09a
jenjereren <jen.alconis@gmail.com>
2023-03-07 09:36:10 +0100

    added favicon + minor map adjustments

--- trip.js
+++ trip.js
@@ -1,9 +1,8 @@
-const map =  L.map("map").setView([49.98, 7.908], 13);
+const map =  L.map("map").setView([49.974, 7.9065], 14);
 
 const CyclOSM = L.tileLayer('https://{s}.tile-cyclosm.openstreetmap.fr/cyclosm/{z}/{x}/{y}.png', {
 	maxZoom: 20,
 	attribution: '<a href="https://github.com/cyclosm/cyclosm-cartocss-style/releases" title="CyclOSM - Open Bicycle render">CyclOSM</a> | Map data: &copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors'
 });
-
 
 CyclOSM.addTo(map);

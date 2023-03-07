b0bef79ab5ac18589bb88df5b73b05939da1bcfe
jenjereren <jen.alconis@gmail.com>
2023-03-07 21:08:38 +0100

    added photos, geojson, minor adjustments

--- trip.js
+++ trip.js
@@ -1,8 +1,26 @@
-const map =  L.map("map").setView([49.974, 7.9065], 14);
+const map = L.map("map").setView([49.986, 7.9], 13);
 
-const CyclOSM = L.tileLayer('https://{s}.tile-cyclosm.openstreetmap.fr/cyclosm/{z}/{x}/{y}.png', {
-	maxZoom: 20,
-	attribution: '<a href="https://github.com/cyclosm/cyclosm-cartocss-style/releases" title="CyclOSM - Open Bicycle render">CyclOSM</a> | Map data: &copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors'
-});
+const CyclOSM = L.tileLayer(
+  "https://{s}.tile-cyclosm.openstreetmap.fr/cyclosm/{z}/{x}/{y}.png",
+  {
+    maxZoom: 20,
+    attribution:
+      '<a href="https://github.com/cyclosm/cyclosm-cartocss-style/releases" title="CyclOSM - Open Bicycle render">CyclOSM</a> | Map data: &copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors',
+  }
+);
 
 CyclOSM.addTo(map);
+
+function onEachFeature(feature, layer) {
+  // does this feature have a property named popupContent?
+  if (feature.properties && feature.properties.popupContent) {
+    layer.bindPopup(feature.properties.popupContent);
+  }
+}
+
+$.getJSON("/data/ringtour.geojson", function (data) {
+  // add GeoJSON layer to the map once the file is loaded
+  L.geoJson(data, {
+    onEachFeature: onEachFeature,
+  }).addTo(map);
+});

ea0fac287dca6ef6fd7c343681581368bd8c02f6
jenjereren <jen.alconis@gmail.com>
2023-03-09 16:43:46 +0100

    clean up

--- trip.js
+++ trip.js
@@ -1,16 +1,5 @@
 const map = L.map("map").setView([49.986, 7.9], 13);
 
-const CyclOSM = L.tileLayer(
-  "https://{s}.tile-cyclosm.openstreetmap.fr/cyclosm/{z}/{x}/{y}.png",
-  {
-    maxZoom: 20,
-    attribution:
-      '<a href="https://github.com/cyclosm/cyclosm-cartocss-style/releases" title="CyclOSM - Open Bicycle render">CyclOSM</a> | Map data: &copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors',
-  }
-);
-
-//CyclOSM.addTo(map);
-
 var osmDE = L.tileLayer("https://{s}.tile.openstreetmap.de/{z}/{x}/{y}.png", {
   maxZoom: 18,
   attribution:
@@ -29,7 +18,6 @@ function onEachFeature(feature, layer) {
 var lineStyle = {
   color: "#ff1900",
   weight: 4,
-  title: "Ringticket track",
   dashArray: "7, 14",
 };
 
@@ -62,13 +50,15 @@ $.getJSON("/data/photo_pts.geojson", function (data) {
         title: feature.properties.name,
         opacity: 0.9,
         icon: customMarker,
-      }).bindPopup(
-        "<img src='/data/" +
-          feature.properties.picture +
-          "' height='550' /> <p>" +
-          feature.properties.desc +
-          "</p>"
-      );
+      })
+        .bindPopup(
+          "<img src='/data/" +
+            feature.properties.picture +
+            "' height='500' /> <p>" +
+            feature.properties.desc +
+            "</p>"
+        )
+        .openPopup();
     },
   }).addTo(map);
 });
